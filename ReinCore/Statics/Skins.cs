4339cb7ba9b362f8044e144bf4fc42dcdea1428d
Reinms <reinmasamuneytstuff@gmail.com>
2020-03-13 18:15:02 -0400

    Started work on profile fixes for skins

--- ReinCore/Statics/Skins.cs
+++ ReinCore/Statics/Skins.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Reflection;
 using BepInEx;
+using RoR2;
 
 namespace ReinCore
 {
     public static class SkinsCore
     {
         public static Boolean loaded { get; internal set; } = false;
+
+
+
+        static SkinsCore()
+        {
+            var allFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+            var parentType = typeof(Loadout.BodyLoadoutManager);
+
+            if( !ReinCore.r2apiExists )
+            {
+
+            }
+        }
+
     }
 }
