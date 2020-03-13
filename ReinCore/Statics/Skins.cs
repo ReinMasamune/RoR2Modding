bea3d7bcc9f4657aa9f0d71f16a0d5effe6fcae9
Reinms <reinmasamuneytstuff@gmail.com>
2020-03-13 18:23:52 -0400

    Started on non-generic accessors

--- ReinCore/Statics/Skins.cs
+++ ReinCore/Statics/Skins.cs
@@ -15,12 +15,24 @@ namespace ReinCore
         {
             var allFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
             var parentType = typeof(Loadout.BodyLoadoutManager);
+            var bodyLoadoutType = parentType.GetNestedType( "BodyLoadout", allFlags );
+
+            
+            
 
             if( !ReinCore.r2apiExists )
             {
 
             }
         }
+
+        private delegate void OrigEnforceValidity( System.Object self );
+        private delegate void OrigEnforceUnlockable( System.Object self, UserProfile userProfile );
+        private delegate void OrigIsSkinValid( System.Object self );
+        private delegate void OrigIsSkilLocked( System.Object self );
+        
+
+
 
     }
 }
