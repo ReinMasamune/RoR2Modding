a3dcc105771d83d2eee3595e2e843ee260dd3701
Reinms <reinmasamuneytstuff@gmail.com>
2020-03-15 13:21:40 -0400

    General plugin setup

--- AlternateArtificer/AlternateArtificer/Main.cs
+++ AlternateArtificer/AlternateArtificer/Main.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using BepInEx;
+using ReinCore;
 
 namespace Rein.AlternateArtificer
 {
+    [BepInDependency( AssemblyLoad.guid )]
+    [BepInPlugin( Main.guid, Main.name, Main.ver )]
     internal partial class Main : BaseUnityPlugin
     {
+        public const String guid = "com.Rein.AltArti";
+        public const String name = "Alternate Artificer";
     }
 }
